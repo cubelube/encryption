a89d90b42d7694b25f23b4eca0c581c9957cb09f
cubelube <114615759+cubelube@users.noreply.github.com>
2023-10-13 14:27:51 -0400

    Add files via upload

---
+++ encryption.rs
@@ -0,0 +1,71 @@
+pub fn encrypt(line: &String) -> String {
+    let mut converted = String::new();
+  
+    for letter in line.chars() {
+      if letter.is_alphabetic() {
+        converted.push(charactercode(letter));
+      } else {
+        converted.push(letter);
+      }
+    }
+  
+    converted
+  }
+
+  fn charactercode(letter: char) -> char {
+    match letter {
+      'A' => 'N',
+      'B' => 'O',
+      'C' => 'P',
+      'D' => 'Q',
+      'E' => 'R',
+      'F' => 'S',
+      'G' => 'T',
+      'H' => 'U',
+      'I' => 'V',
+      'J' => 'W',
+      'K' => 'X',
+      'L' => 'Y',
+      'M' => 'Z',
+      'N' => 'A',
+      'O' => 'B',
+      'P' => 'C',
+      'Q' => 'D',
+      'R' => 'E',
+      'S' => 'F',
+      'T' => 'G',
+      'U' => 'H',
+      'V' => 'I',
+      'W' => 'J',
+      'X' => 'K',
+      'Y' => 'L',
+      'Z' => 'M',
+      'a' => 'n',
+      'b' => 'o',
+      'c' => 'p',
+      'd' => 'q',
+      'e' => 'r',
+      'f' => 's',
+      'g' => 't',
+      'h' => 'u',
+      'i' => 'v',
+      'j' => 'w',
+      'k' => 'x',
+      'l' => 'y',
+      'm' => 'z',
+      'n' => 'a',
+      'o' => 'b',
+      'p' => 'c',
+      'q' => 'd',
+      'r' => 'e',
+      's' => 'f',
+      't' => 'g',
+      'u' => 'h',
+      'v' => 'i',
+      'w' => 'j',
+      'x' => 'k',
+      'y' => 'l',
+      'z' => 'm',
+      _ => ' '
+    }
+  }
